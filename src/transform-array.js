b04ad6e196bb9d19d09cfbc8bc4a362e6c7dd0ea
EkaterinaMosina <k.bedenko@mail.ru>
2021-02-02 16:22:14 +0300

    feat: implement 4 functions:cats,team,Hanoi,transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) throw "Error";
+  let arr2=[];
+  for (let i=0; i<arr.length;i++) {
+   if (arr[i]=="--double-next") {
+      if (arr[i+1]!=undefined) {
+        arr2.push(arr[i+1]);
+        
+    }
+  }
+  else if (arr[i]=="--double-prev") {
+    if (arr[i-1]!=undefined && arr[i-2]!="--discard-next") {
+      arr2.push(arr[i-1]);
+      
+  }
+  }
+  else {
+     if(arr[i]!='--discard-prev' && arr[i]!='--discard-next' && arr[i+1]!='--discard-prev' && arr[i-1]!='--discard-next') {
+    arr2.push(arr[i]);
+  }
+  }
+}
+  return arr2;
+}
